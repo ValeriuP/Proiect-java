bdb1ba6cc8e9e9a74ddd8943718dd4495575b7a5
ValeriuP <pacuraruvaleriu@gmail.com>
2024-04-15 09:49:30 +0300

    completare

--- home.js
+++ home.js
@@ -1,4 +1,19 @@
-const storedData=JSON.parse(localStorage.getItem("user-1"));
-const userName=document.getElementById("user_name");
-userName.innerText=`Hello,${storedData[firstName]}`;
-console.log(storedData);
+// const storedData=JSON.parse(localStorage.getItem("userSave"));
+// const userName=document.getElementById("user_name");
+// // firstName=firstName.value;
+// // userName.innerText=`Hello,${storedData[firstName]}`;
+// // console.log(storedData);
+
+
+// functia butonului login
+function logout(){
+    window.location.href="login.html"
+}
+// let addNewFlat=document.getElementById("container");
+
+function addNewFlat(){
+    const form=document.getElementById("form");
+    form.style.display="flex";
+    const container=document.getElementById(".container");
+    container.classList.add("blur-container")
+}
